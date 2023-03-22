7797488be937448142966c76726d70e7d585ee1a
Felipe Portari <felipe.portari7@gmail.com>
2023-03-22 12:01:40 -0300

    super treino

--- treino4.php
+++ treino4.php
@@ -1,24 +1,24 @@
 <?php
 
-$pati = 5;
+$pati = readline('Digite a quantidade de patinhos: ' . PHP_EOL);
 
 
 for ($i = $pati; $i > 0; $i--) {
     $e = $i - 1;
     if ($i == 1) {
         echo "$i patinho foi passear
-    Além das montanhas para brincar
-     A mamãe gritou: Quá, quá, quá, quá
-    Mas só $e patinho voltou de lá" . PHP_EOL;
+        Além das montanhas para brincar
+        A mamãe gritou: Quá, quá, quá, quá
+        Mas nenhum patinho voltou de lá..." . PHP_EOL;
     } elseif ($i == 2) {
         echo "$i patinhos foram passear
         Além das montanhas para brincar
         A mamãe gritou: Quá, quá, quá, quá
-        Mas nenhum patinho voltou de lá..." . PHP_EOL;
+        Mas só $e patinho voltou de lá" . PHP_EOL;
     } else {
         echo "$i patinhos foram passear
-            Além das montanhas para brincar
-            A mamãe gritou: Quá, quá, quá, quá
-            Mas só $e patinhos voltaram de lá" . PHP_EOL;
+        Além das montanhas para brincar
+        A mamãe gritou: Quá, quá, quá, quá
+        Mas só $e patinhos voltaram de lá" . PHP_EOL;
     }
 }
